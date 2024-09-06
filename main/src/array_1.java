eab845d33c24857792e83e049ebf171f3ef18f30
Aditya <adityaashal512@gmail.com>
2024-09-06 20:31:58 +0530

    codes

--- main/src/array_1.java
+++ main/src/array_1.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
+
 public class array_1 {
     public static void main(String[] args) {
-        int arry[]={10,20,30,40,50};
-        //arry[2]=80;
-        for(int i=0;i<arry.length;i++){
-            System.out.println(arry[i]);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the size : ");
+        int size = sc.nextInt();
+
+        int total=0;
+        int arry[]= new int[size];
+
+        for(int i=0;i<size;i++){
+            System.out.println("Enter the "+(i+1)+"th element");
+            arry[i] = sc.nextInt();
         }
+
+        for(int i=0;i<size;i++){
+            System.out.println(arry[i]);
+            total += arry[i];
+        }
+        System.out.println("Total is "+total);
     }
 }
